4aac76301cf9ac2657f3310c584eced261b8491a
b-hyoung <youqlrqod@gmail.com>
2025-02-13 13:10:31 +0900

    Create Navbar BlogName

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react'
+import {Routes , Route} from 'react-router-dom'
 import './App.css';
+import Navbar from './Header/Navbar.js';
 
 function App() {
   return (
     <div className="App">
-        
+      <Navbar />
+      <Routes>
+        <Route />
+        </Routes>
     </div>
   );
 }
--- frontend/src/Header/Navbar.js
+++ frontend/src/Header/Navbar.js
@@ -1,22 +1,13 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
     return (
         <div className='Header_NavBar'>
             {/* 블로그 이름 */}
             <div className='Header_BlogName'>
-                <div>Who i am?</div>
+                Who I Am?
             </div>
-            {/*
-                디자인 변경으로인한 보류 탑바를 이용한 사이트를 만들 때 재사용 할 것
-            <div className='Header_Category'>
-                <div>Study</div>
-                <div>Item</div>
-                <div>I'M</div>
-            </div>
-            <div className='Header_Login'>
-                <div>Who are you?</div>
-            </div> */}
         </div>
     )
 }
