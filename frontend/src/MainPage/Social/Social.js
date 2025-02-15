15f32bc57f777824eac7ca129321a1947d686b2e
b-hyoung <youqlrqod@gmail.com>
2025-02-16 00:00:00 +0900

    Create Social

---
+++ frontend/src/MainPage/Social/Social.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Social.css'
+
+function Social() {
+  return (
+    <div className='Social'>
+      <h1>Social</h1>
+      <img src='https://velog.io/@h-young/posts'/><a>Git : https://github.com/b-hyoung</a>
+      <a>Email : youqlrqod@gmail.com</a>
+      <a>Notion : htts://notion.so/b-hyoung</a>
+      <a>Velog : https://velog.io/@h-young/posts</a>
+    </div>
+  )
+}
+
+export default Social
