4bfa2dbe4eda9909dafd6db1afe087a1aeffaded
b-hyoung <youqlrqod@gmail.com>
2025-02-18 01:07:33 +0900

    Navba LogoImg

--- frontend/src/Header/Navbar.js
+++ frontend/src/Header/Navbar.js
@@ -3,11 +3,14 @@ import './Navbar.css'
 import ScrollNavbar from './Section/ScrollNavbar.js'
 
 function Navbar() {
+
+    const logoUrl = process.env.PUBLIC_URL+"/img/Logo/BlogLogo.png"
+
     return (
         <div className='Header_NavBar'>
             {/* 블로그 이름 */}
             <div className='Header_BlogName'>
-                Who I Am?
+                <img className='Nav_Logo' src={logoUrl}/>
             </div>
             <div>
             </div>
