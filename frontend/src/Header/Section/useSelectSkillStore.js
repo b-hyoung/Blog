66b273e4861b3372701e53087a3caa642b885b25
b-hyoung <youqlrqod@gmail.com>
2025-02-25 23:04:36 +0900

    create use Zustand (select Skill)

---
+++ frontend/src/Header/Section/useSelectSkillStore.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {create} from 'zustand'
+
+
+const useSelectSkillStore = create((set) => (
+{
+        skill : '',
+        setSkill : (selectValue) => set({skill : selectValue}),
+        skills:[],
+        setSkills : (newSelectSkill) =>
+            set((prev) => ({
+                skills : [...prev.skills,newSelectSkill],
+            })),
+    }))
+
+export default useSelectSkillStore;
