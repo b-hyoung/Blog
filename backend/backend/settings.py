9f412f75bc9d35907f0167c2251e957962f740cc
b-hyoung <youqlrqod@gmail.com>
2025-03-15 17:16:04 +0900

    fix DB Info

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_db',
-        'USER': 'kimbobnim',
-        'PASSWORD': 'rlaqkq1125',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '3306',
     }
 }
