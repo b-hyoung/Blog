9f771e3b62131ac1ffc131230fb7887dcbdd6c1a
b-hyoung <youqlrqod@gmail.com>
2025-03-26 03:30:03 +0900

    delete

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,4 +11,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-#"Bearer bbca229e630aab705ed95de77107a48ca6fe8ac0"
